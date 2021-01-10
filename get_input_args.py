18debc2a89f134a0e748af3441b1a379c6ee3072
milaek <56858892+milaek@users.noreply.github.com>
2021-01-09 22:50:12 -0800

    Fill out get_input_args function
    Feat added. Used argparse lib to add functionality to get_input_args, as well as set default values.

--- get_input_args.py
+++ get_input_args.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/get_input_args.py
 #                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                   
+# PROGRAMMER: Kate Matheson
+# DATE CREATED: 1/8/21
 # REVISED DATE: 
 # PURPOSE: Create a function that retrieves the following 3 command line inputs 
 #          from the user using the Argparse Python module. If the user fails to 
@@ -17,10 +17,7 @@
 # Imports python modules
 import argparse
 
-# TODO 1: Define get_input_args function below please be certain to replace None
-#       in the return statement with parser.parse_args() parsed argument 
-#       collection that you created with this function
-# 
+
 def get_input_args():
     """
     Retrieves and parses the 3 command line arguments provided by the user when
@@ -39,10 +36,16 @@ def get_input_args():
      parse_args() -data structure that stores the command line arguments object  
     """
     # Create Parse using ArgumentParser
-    
-    # Create 3 command line arguments as mentioned above using add_argument() from ArguementParser method
-    
-    
-    # Replace None with parser.parse_args() parsed argument collection that 
-    # you created with this function 
-    return None
+    parser = argparse.ArgumentParser()
+
+    # Argument 1: that's a path to a folder
+    parser.add_argument('--dir', type=str, default='pet_images/',
+                        help='path to the folder of pet images')
+
+    # Argument 2:  CNN model name
+    parser.add_argument('--arch', type=str, defailt='vgg', help='type of CNN model architecture to use for processing')
+
+    # Argument 3: text file of dog names
+    parser.add_argument('--dogfile', type=str, default='dognames.txt', help='file of dog names')
+
+    return parser.parse_args()
