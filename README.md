975ea9f2a4feb9d03e099322ebb6345b34cbfef8
milaek <56858892+milaek@users.noreply.github.com>
2021-01-10 21:40:15 -0800

    Add feat get_pet_labels to get_pet_labels.py
    Added code for get_pet_labels. Also adjusted the readme with updated information

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Dog Image Classifier
  A project for Udacity's AI Programming with Python Nanodegree course. This project uses various image classifiers to identify images with dogs as well as their breeds. Base code for the classifier and tests written by Udacity.
+
+
+# Motivation
+Written as part of the AI Programming with Python Nanodegree Program provided by Udacity.
+
+# Installation
+Please install the following modules for python:
+    - torchvision
+
+# Tests
+TBD
+
+# How to use?
+TBD
+
+# Credits
+Credit for Classifier code and project idea to Udacity and the AI Programming for Python Nandegree.
--- get_pet_labels.py
+++ get_pet_labels.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/get_pet_labels.py
 #                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                  
+# PROGRAMMER: Kate Matheson
+# DATE CREATED: 1/10/21
 # REVISED DATE: 
 # PURPOSE: Create the function get_pet_labels that creates the pet labels from 
 #          the image's filename. This function inputs: 
@@ -19,10 +19,7 @@
 # Imports python modules
 from os import listdir
 
-# TODO 2: Define get_pet_labels function below please be certain to replace None
-#       in the return statement with results_dic dictionary that you create 
-#       with this function
-# 
+
 def get_pet_labels(image_dir):
     """
     Creates a dictionary of pet labels (results_dic) based upon the filenames 
@@ -40,6 +37,13 @@ def get_pet_labels(image_dir):
       List. The list contains for following item:
          index 0 = pet image label (string)
     """
-    # Replace None with the results_dic dictionary that you created with this
-    # function
-    return None
+    # set up output dict
+    results_dic = {}
+
+    # translate each file into the breed name and set to file in dic
+    for file in listdir(image_dir):
+        name = file.translate(file.maketrans("_-", "  ", "0123456789.")).strip(" ").lower()
+        results_dic[file] = [name[:-4]]
+
+    return results_dic
+
